e9b4280a12883de26b03b127988bef988871b3d6
brianko <kobj11@gmail.com>
2022-02-04 23:12:15 -0500

    commit tree 78f9d0e17fa725fd3492c4fe8611e5f0e32540a2

--- index.js
+++ index.js
@@ -8,6 +8,61 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+const createEmployeeRecord = function (employee) {
+    let employeeRecord = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+   }
+   return employeeRecord
+}
+
+const createEmployeeRecords = function (record) {
+    return record.map(employee => createEmployeeRecord(employee))
+}
+
+const createTimeInEvent = function (timeInString) {
+    let timeInArray = timeInString.split(" ")
+    
+    let timeInEvent = {
+        type: "TimeIn",
+        hour: Number(timeInArray[1]),
+        date: timeInArray[0]
+    }
+    
+    
+    this.timeInEvents.push(timeInEvent)
+    return this
+}
+
+const createTimeOutEvent = function (timeInString) {
+    let timeInArray = timeInString.split(" ")
+    
+    let timeOutEvent = {
+        type: "TimeOut",
+        hour: Number(timeInArray[1]),
+        date: timeInArray[0]
+    }
+    
+    
+    this.timeOutEvents.push(timeOutEvent)
+    return this
+}
+
+const hoursWorkedOnDate = function (date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date)
+    const timeOut = this.timeOutEvents.find(event => event.date === date)
+    
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+const wagesEarnedOnDate = function (date) {
+    const hours = hoursWorkedOnDate.call(this, date)
+    return this.payPerHour * hours
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -21,3 +76,11 @@ const allWagesFor = function () {
     return payable
 }
 
+const calculatePayroll = function (employeeRecords) {
+   const record = employeeRecords.map(employee => allWagesFor.call(employee))
+    return record.reduce((acc,total) => acc+total)
+}   
+
+const findEmployeeByFirstName = function (employees, firstNameString) {
+    return employees.find(employee => employee.firstName === firstNameString)
+}
